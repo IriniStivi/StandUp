3983cc647c331b65f49727c7579fcd05bb210d8e
Irini Stivaktakis <irini.stivaktakis@googlemail.com>
2019-10-26 08:47:35 +0200

    worked on file architecture

--- js/standup.js
+++ js/standup.js
@@ -1,5 +1,5 @@
 function go() {
-    new Audio('glass_ping-Go445-1207030150.mp3').play();
+    new Audio('../mp3/glass_ping-Go445-1207030150.mp3').play();
 
     function startTimer() {
 
@@ -7,10 +7,10 @@ function go() {
 
         if (text === "Stand up!") {
             document.getElementById("button").innerHTML = "Sit down.";
-            new Audio('descending_craft-Sonidor-991848481.mp3').play();
+            new Audio('../mp3/descending_craft-Sonidor-991848481.mp3').play();
         } else {
             document.getElementById("button").innerHTML = "Stand up!";
-            new Audio('UFO_Takeoff-Sonidor-1604321570.mp3').play();
+            new Audio('../mp3/UFO_Takeoff-Sonidor-1604321570.mp3').play();
         }
     }
     setInterval(startTimer, 600000);
