597d3d39bb80e91aabcb2b3e1d88759651147add
Irini Stivaktakis <irini.stivaktakis@googlemail.com>
2019-10-26 13:35:38 +0200

    added input to choose duration, added Bootstrap

--- js/standup.js
+++ js/standup.js
@@ -1,9 +1,18 @@
+
+
 function hide() {
     var element = document.getElementById("start");
     element.classList.add("hide");
   }
 
+function set() {
+    t = parseInt((document.getElementById("duration").value) * 60000);
+    console.log("Input: " + t); // to make sure user input got logged
+    return t;
+}
+
 function start() {
+    console.log(t);
     new Audio('../mp3/Bike Horn-SoundBible.com-602544869.mp3').play();
 
     document.getElementById("display").innerHTML = "Stand up!";
@@ -21,5 +30,5 @@ function start() {
             new Audio('../mp3/UFO_Takeoff-Sonidor-1604321570.mp3').play();
         }
     }
-    setInterval(startTimer, 5000);
+    setInterval(startTimer, t);
 }
