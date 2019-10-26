79d3d939975c5121af4ded422e323ca7c28d461b
Irini Stivaktakis <irini.stivaktakis@googlemail.com>
2019-10-26 11:09:30 +0200

    changed sound styling

--- js/standup.js
+++ js/standup.js
@@ -1,17 +1,18 @@
-function go() {
-    new Audio('../mp3/glass_ping-Go445-1207030150.mp3').play();
+function start() {
+    new Audio('../mp3/Bike Horn-SoundBible.com-602544869.mp3').play();
 
-    document.getElementById("button").innerHTML = "Stand up!";
+    document.getElementById("display").innerHTML = "Stand up!";
+    
 
     function startTimer() {
 
-        var text = document.getElementById("button").innerHTML;
+        var text = document.getElementById("display").innerHTML;
 
         if (text === "Stand up!") {
-            document.getElementById("button").innerHTML = "Sit down.";
+            document.getElementById("display").innerHTML = "Sit down.";
             new Audio('../mp3/descending_craft-Sonidor-991848481.mp3').play();
         } else {
-            document.getElementById("button").innerHTML = "Stand up!";
+            document.getElementById("display").innerHTML = "Stand up!";
             new Audio('../mp3/UFO_Takeoff-Sonidor-1604321570.mp3').play();
         }
     }
